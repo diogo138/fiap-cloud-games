d4fd83006494a54c04f8efb80438418389f410d3
Glayson Sergio <glaysons@gmail.com>
2026-04-24 20:20:13 -0400

    Criação de propriedade padronizada para consulta do contexto de segurança atual

--- src/FiapCloudGames/FIAP.FCG.Web.API/Controllers/PadraoController.cs
+++ src/FiapCloudGames/FIAP.FCG.Web.API/Controllers/PadraoController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using FIAP.FCG.Application.Autenticacao.Dtos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace FIAP.FCG.Web.API.Controllers;
 
@@ -9,4 +12,18 @@ namespace FIAP.FCG.Web.API.Controllers;
 public class PadraoController : ControllerBase
 {
 
+	private ContextoAtual? _contexto;
+
+	protected ContextoAtual ContextoSeguranca
+	{
+		get => (_contexto ??= ObterContextoSeguranca());
+	}
+
+	private ContextoAtual ObterContextoSeguranca()
+	{
+		var usuarioId = int.Parse(User.Claims.First(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value);
+		var email = User.Claims.First(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").Value;
+		var administrador = (User.Claims.First(c => c.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role").Value == "Administrador");
+		return new ContextoAtual(usuarioId, email, administrador);
+	}
 }
