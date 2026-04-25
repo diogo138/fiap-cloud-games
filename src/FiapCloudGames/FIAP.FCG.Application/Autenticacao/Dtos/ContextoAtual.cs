08340f3757cc90db060942a09feff0182c8da59e
Glayson Sergio <glaysons@gmail.com>
2026-04-24 20:17:50 -0400

    Criação de classe pra gerenciamento do contexto atual

---
+++ src/FiapCloudGames/FIAP.FCG.Application/Autenticacao/Dtos/ContextoAtual.cs
@@ -0,0 +1,20 @@
+﻿namespace FIAP.FCG.Application.Autenticacao.Dtos
+{
+	public record ContextoAtual
+	{
+
+		public int Usuario { get; }
+
+		public string Email { get; }
+
+		public bool Administrador { get; }
+
+		public ContextoAtual(int usuario, string email, bool administrador)
+		{
+			Usuario = usuario;
+			Email = email;
+			Administrador = administrador;
+		}
+
+	}
+}
